b3d76b7dadb9887fff62abaa3f09fdc6cf404b7a
llreebor <timoschuk.roman@gmail.com>
2024-10-04 17:26:26 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-red-400">asdasd</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
